ac879580e22ca255e6e803cf7947fa7a0592f520
Dima Marhitych <uzhdimka@gmail.com>
2023-10-02 22:25:21 +0200

    Update document.go

--- src/backend/document/document.go
+++ src/backend/document/document.go
@@ -2,26 +2,26 @@ package document
 
 import "syscall/js"
 
-var Body = _body{}
+var Body = body{}
 
-type _body struct{}
+type body struct{}
 
-type _element struct {
+type element struct {
 	el    js.Value
 	Style js.Value
 }
 
-func CreateElement(element string) _element {
-	cd := js.Global().Get("document").Call("createElement", element)
+func CreateElement(element_str string) element {
+	cd := js.Global().Get("document").Call("createElement", element_str)
 	style := cd.Get("style")
-	return _element{cd, style}
+	return element{cd, style}
 }
 
-func (e *_element) Set(key string, value interface{}) {
+func (e *element) Set(key string, value interface{}) {
 	e.el.Set(key, value)
 }
 
-func (b *_body) AppendChild(element _element) {
-	appendChild := element.el
+func (b *body) AppendChild(el element) {
+	appendChild := el.el
 	js.Global().Get("document").Get("body").Call("appendChild", appendChild)
 }
