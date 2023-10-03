ee44a6902e3b377e5b9671c0e6be1481d788b12b
Dima Marhitych <uzhdimka@gmail.com>
2023-10-03 22:05:01 +0200

    Update main example

--- example/main.go
+++ example/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "webzen/src/backend/console"
+import (
+	// Import the webzen package
+	"webzen/src/webzen"
+
+	// Import the webzen draw package
+	"webzen/src/webzen/draw"
+)
 
 func main() {
-	console.Log("Hello, World!")
+	// Initialize webzen
+	webzen.Init()
 
+	// Fill background
+	draw.FillBackground([4]int{255, 255, 188, 255})
+
+	// Draw text on the screen
+	draw.DrawText("Hello under the world!", 21, 100, 100)
+
+	// Draw a rectangle on the screen
+	draw.DrawRect(50, 500, 400, 400, [4]int{146, 255, 123, 255})
+
+	// Draw a second rectangle on the screen
+	draw.DrawRect(200, 200, 100, 400, [4]int{146, 255, 123, 255})
 }
