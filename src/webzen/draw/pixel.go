4fb73887cca23a910b2bc651c8dd9e40d37162ef
Dima Marhitych <uzhdimka@gmail.com>
2023-10-05 18:49:53 +0200

    Create SetPixel

---
+++ src/webzen/draw/pixel.go
@@ -0,0 +1,25 @@
+//go:build js && wasm
+// +build js,wasm
+
+package draw
+
+import (
+	"strconv"
+
+	"github.com/dimkauzh/webzen/src/backend/document"
+)
+
+func SetPixel(x, y int, color [4]int) {
+	canvas := document.GetElementById("canvas")
+	// Get the canvas context
+	context := canvas.GetContext("2d")
+
+	// Draw the rectangle
+	rgba := "rgba(" +
+		strconv.Itoa(color[0]) + "," +
+		strconv.Itoa(color[1]) + "," +
+		strconv.Itoa(color[2]) + "," +
+		strconv.FormatFloat(float64(color[3])/255.0, 'f', -1, 64) + ")"
+	context.Set("fillStyle", rgba)
+	context.FillRect(x, y, 1, 1) // Set the rectangle dimensions as needed
+}
