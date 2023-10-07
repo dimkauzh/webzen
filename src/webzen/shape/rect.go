681126614bba5d74fd3733642f383485400e1b77
Dima Marhitych <uzhdimka@gmail.com>
2023-10-07 19:42:47 +0200

    Added Pre-Setting a rectangle

---
+++ src/webzen/shape/rect.go
@@ -0,0 +1,31 @@
+package shape
+
+import (
+	"github.com/dimkauzh/webzen/src/backend/colors"
+	"github.com/dimkauzh/webzen/src/backend/document"
+)
+
+
+type Rect struct {
+  x float64
+  y float64
+  width float64
+  height float64
+  color [4]int
+
+}
+
+func NewRect(x, y, width, height float64, color [4]int) Rect {
+  return Rect{x, y, width, height, color}
+}
+
+func (r *Rect) Draw() {
+  canvas := document.GetElementById("canvas")
+	// Get the canvas context
+	context := canvas.GetContext("2d")
+
+	// Draw the rectangle
+	rgba := colors.GetRGBA(r.color)
+	context.Set("fillStyle", rgba)
+	context.FillRect(r.x, r.y, r.width, r.height) 
+}
