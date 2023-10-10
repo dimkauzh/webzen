07d37689deab0a18d50a302754c232e0126f8589
Dima Marhitych <uzhdimka@gmail.com>
2023-10-10 17:11:40 +0200

    Entity system is done

--- src/webzen/entity/entity.go
+++ src/webzen/entity/entity.go
@@ -2,3 +2,29 @@
 // +build js,wasm
 
 package entity
+
+import (
+  "github.com/dimkauzh/webzen/src/webzen/image"
+  rect "github.com/dimkauzh/webzen/src/webzen/shape"
+)
+
+type Entity struct {
+  x      int
+  y      int
+  width  int
+  height int
+  image  image.Image
+  rect   rect.Rect
+}
+
+func NewEntity(x int, y int, width int, height int) Entity {
+  return Entity{x, y, width, height, nil, nil}
+}
+
+func (e *Entity) SetImage(image image.Image) {
+  e.image = image
+}
+
+func (e *Entity) SetRect(rect rect.Rect) {
+  e.rect = rect
+}
